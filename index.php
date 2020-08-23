e7ad7d7f56f539bdebc4adb8d212904735f9bd71
gabrielsouza988 <gabriel.souza988@hotmail.com>
2020-08-23 15:49:55 -0300

    basic structure html
    this  is first basic structure html

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LaddingPage</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<meta name="keywords" content="DankiCode,LaddingPage,skills,developer,meu-site">
+	<meta name="description" content="this is a project on a course from DankiCode. Is a project to show my skills as a programmer!">
+</head>
+<body>
+	<header>
+		<div class="logo">Logotipo</div>
+
+		<!------------------------
+			NAVBAR DESKTOP
+		 ------------------------->
+		<nav class="desktop">
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Serviços</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+		</nav>
+
+		<!------------------------
+			NAVBAR Mobile
+		 ------------------------->
+		<nav class="mobile">
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Serviços</a></li>
+				<li><a href="">Contato</a></li>
+			</ul>
+		</nav>
+
+	</header>
+
+	<!------------------------
+		Main Banner
+	------------------------->
+	<section class="main-banner">
+		<form method="post">
+			<h2>Qual seu melhor e-mail?</h2>
+			<input type="email"name ="email" required>
+			<input type="submit" name="acao" value="Cadastrar!">
+		</form>
+	</section>
+
+	<!------------------------
+		Author Description
+	------------------------->
+	<section class="author-description">
+		<div class="w50">
+			<h2>Gabriel de Oliveira Souza</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat.</p>
+		</div>
+		<div class="w50">
+			<img src="assets/img/profile.png">	
+		</div>
+	</section>
+
+</body>
+</html>
