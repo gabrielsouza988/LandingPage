01f8319901dfc0aa02f23809e20d2f67394c8321
gabrielsouza988 <gabriel.souza988@hotmail.com>
2020-08-24 11:37:19 -0300

    markings  html completed
    markings  html completed

--- index.php
+++ index.php
@@ -9,64 +9,162 @@
 </head>
 <body>
 	<header>
-		<div class="logo">Logotipo</div>
+		<div class="center">
+			<div class="logo">Logotipo</div>
 
-		<!------------------------
-			NAVBAR DESKTOP
-		 ------------------------->
-		<nav class="desktop">
-			<ul>
-				<li><a href="">Home</a></li>
-				<li><a href="">Sobre</a></li>
-				<li><a href="">Serviços</a></li>
-				<li><a href="">Contato</a></li>
-			</ul>
-		</nav>
+			<!------------------------
+				NAVBAR DESKTOP
+			 ------------------------->
+			<nav class="desktop">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
 
-		<!------------------------
-			NAVBAR Mobile
-		 ------------------------->
-		<nav class="mobile">
-			<ul>
-				<li><a href="">Home</a></li>
-				<li><a href="">Sobre</a></li>
-				<li><a href="">Serviços</a></li>
-				<li><a href="">Contato</a></li>
-			</ul>
-		</nav>
-
+			<!------------------------
+				NAVBAR Mobile
+			 ------------------------->
+			<nav class="mobile">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Sobre</a></li>
+					<li><a href="">Serviços</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+		</div>
 	</header>
 
 	<!------------------------
 		Main Banner
 	------------------------->
 	<section class="main-banner">
-		<form method="post">
-			<h2>Qual seu melhor e-mail?</h2>
-			<input type="email"name ="email" required>
-			<input type="submit" name="acao" value="Cadastrar!">
-		</form>
+		<div class="center">
+			<form method="post">
+				<h2>Qual seu melhor e-mail?</h2>
+				<input type="email"name ="email" required>
+				<input type="submit" name="acao" value="Cadastrar!">
+			</form>
+		</div>
 	</section>
 
 	<!------------------------
 		Author Description
 	------------------------->
 	<section class="author-description">
-		<div class="w50">
-			<h2>Gabriel de Oliveira Souza</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat.</p>
-		</div>
-		<div class="w50">
-			<img src="assets/img/profile.png">	
+		<div class="center">
+			<div class="w50">
+				<h2>Gabriel de Oliveira Souza</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>
+			</div>
+			<div class="w50">
+				<img src="assets/img/profile.png">	
+			</div>
 		</div>
 	</section>
+
+	<!------------------------
+		Skills
+	------------------------->
+	<section class="skills">
+		<div class="center">
+			<h2 class="title">Especialidades</h2>
+			<div class="box-skills">
+				<h3>Icone 1</h3>
+				<h3>CSS3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>	
+			</div>
+			<div class="box-skills">
+				<h3>Icone 2</h3>
+				<h3>HTML5</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>	
+			</div>
+			<div class="box-skills">
+				<h3>Icone 3</h3>
+				<h3>JavaScript</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat.</p>	
+			</div>
+		</div>
+	</section>
+
+	<!------------------------
+		Extras
+	------------------------->
+	<section class="extras">
+		<div class="center">
+			<div class=w50>
+				<h2 class="title">Depoimentos</h2>
+				<div class="depoiment-single">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>	
+					<p class="author-name">Lorem Ipsum</p>
+				</div>
+				<div class="depoiment-single">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>	
+					<p class="author-name">Lorem Ipsum</p>
+				</div>
+				<div class="depoiment-single">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>	
+					<p class="author-name">Lorem Ipsum</p>
+				</div>
+			</div>
+			<div class=w50>
+				<h2 class="title">Serviços</h2>
+				<div class="services">
+					<ul>
+						<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.</li>
+						<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.</li>
+						<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+						consequat.</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!------------------------
+		Footer
+	------------------------->
+	<footer>
+		<div class="center">
+			<p>Todos os direitos rezervados</p>	
+		</div>
+	</footer>
 
 </body>
 </html>
