7f17358a733eec4d961bb48018c581fa623de3e3
gabrielsouza988 <gabriel.souza988@hotmail.com>
2020-08-25 12:28:23 -0300

    almost ready style
    almost ready style

--- index.php
+++ index.php
@@ -32,6 +32,9 @@
 				NAVBAR Mobile
 			 ------------------------->
 			<nav class="mobile right">
+				<div class="button-menu-mobile">
+					<i class="fa fa-bars"></i>
+				</div>
 				<ul>
 					<li><a href="">Home</a></li>
 					<li><a href="">Sobre</a></li>
@@ -66,11 +69,24 @@
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat.</p>
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat.</p>
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur.</p>
 			</div>
 			<div class="w50 left">
 				<img src="assets/img/profile.png">	
@@ -87,7 +103,7 @@
 			<h2 class="title">Especialidades</h2>
 			<div class="w33 left box-skills">
 				<h3><i class="fab fa-css3-alt"></i></h3>
-				<h3>CSS3</h3>
+				<h4>CSS3</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -95,7 +111,7 @@
 			</div>
 			<div class="w33 left box-skills">
 				<h3><i class="fab fa-html5"></i></h3>
-				<h3>HTML5</h3>
+				<h4>HTML5</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -103,12 +119,13 @@
 			</div>
 			<div class="w33 left box-skills">
 				<h3><i class="fab fa-js"></i></h3>
-				<h3>JavaScript</h3>
+				<h4>JavaScript</h4>
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat.</p>	
 			</div>
+		<div class="clear"></div>
 		</div>
 	</section>
 
